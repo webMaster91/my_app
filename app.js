8be93d9d5244de05eaa587b224163a88fd2fbab3
webMaster91 <ytw9103@naver.com>
2017-10-12 15:43:43 +0900

    html first insert

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World~!');
-});
+app.use( express.static( path.join( __dirname + '/public' ) ) );
+console.log(__dirname);
+// app.get('/', function (req, res) {
+//     res.send('Hello World~!');
+// });
 
 app.listen(3000, function () {
     console.log('Server On!');
